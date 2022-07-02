2f9b23a11c6234a7349f0c146857f23cd2747f85
khvci <88493840+khvci@users.noreply.github.com>
2022-07-02 22:58:53 +0300

    create RegistrationController

--- src/main/java/kahveci/co/customuserstore/testController.java
+++ src/main/java/kahveci/co/customuserstore/testController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
+public class testController {
     @GetMapping("/test")
     public String test() {
         return "/test endpoint is accessed.";
