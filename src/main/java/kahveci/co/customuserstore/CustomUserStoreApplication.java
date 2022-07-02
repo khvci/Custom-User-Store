3d3c4c50f9f93c595c2aa6dc668ff7b684aacd65
khvci <88493840+khvci@users.noreply.github.com>
2022-07-02 22:20:34 +0300

    initial commit

---
+++ src/main/java/kahveci/co/customuserstore/CustomUserStoreApplication.java
@@ -0,0 +1,13 @@
+package kahveci.co.customuserstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomUserStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomUserStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kahveci/co/customuserstore/CustomUserStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kahveci.co.customuserstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomUserStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
