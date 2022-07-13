94c3b6bddb5ce9f9a85e673170298b17829612ff
khvci <88493840+khvci@users.noreply.github.com>
2022-07-13 20:21:11 +0300

    fix wrong interface implementation

--- src/main/java/kahveci/co/customuserstore/WebSecurityConfigurerImpl.java
+++ src/main/java/kahveci/co/customuserstore/WebSecurityConfigurerImpl.java
@@ -2,11 +2,13 @@ package kahveci.co.customuserstore;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-public class WebSecurityConfigurerImpl extends WebSecurityConfiguration {
+@EnableWebSecurity
+public class WebSecurityConfigurerImpl extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
