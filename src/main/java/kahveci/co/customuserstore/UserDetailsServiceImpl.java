53e7b07c39e7b3cd1ecd60466342ded36865f77a
khvci <88493840+khvci@users.noreply.github.com>
2022-07-14 19:15:21 +0300

    create UserDetailsServiceImpl

---
+++ src/main/java/kahveci/co/customuserstore/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package kahveci.co.customuserstore;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        User user = userRepo.findUserByUsername(userName);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found: " + userName);
+        }
+
+        return new UserDetailsImpl(user);
+    }
+}
